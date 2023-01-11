c2db7b10bd2ecca136240586e59a250b9a02f06b
EC2 Default User <ec2-user@ip-172-31-39-65.ap-northeast-1.compute.internal>
2023-01-11 13:41:05 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,59 @@
 class BooksController < ApplicationController
-  def top
+  def new
+   @book = Book.new
+  end
+
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "投稿に成功しました"
+    else
+      @books = Book.all
+      render :index
+
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+
+  end
+
+  def show
+    @book = Book.find(params[:id])
+
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+
+  end
+
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+    flash[:update] = "更新に成功しました"
+  end
+
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+    flash[:destroy] = "削除が成功しました"
+  end
+
+
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get '/' => 'books#top'
-  post 'books' => 'lists#create'
-  get 'books' => 'lists#index'
-  get 'books/:id' => 'lists#show', as: 'list'
-  get 'books/:id/edit' => 'lists#edit', as: 'edit_list'
-  delete 'books/:id' => 'lists#destroy', as: 'destroy_list'
-  patch 'books/:id' => 'lists#update',as: 'update_list'
+  root to: "books#top"
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  patch 'books/:id' => 'books#update',as: 'update_book'
 
 end
